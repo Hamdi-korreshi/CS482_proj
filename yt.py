458f9d2837ed2871b51c3a24b4c9a780fea5190a
Hamdi-korreshi <hamdikorreshi@gmail.com>
2023-10-28 13:04:47 -0400

    Changed the library from pytube to pytubefix, downloaded video and captions

--- yt.py
+++ yt.py
@@ -1,6 +1,8 @@
-from pytube import YouTube
+from pytubefix import YouTube
 
 video1 = YouTube("https://www.youtube.com/watch?v=kRHhZ8l9RMQ")
+title = video1.title + " Captions"
+file1 = open(title, "w")
 caption = video1.captions['en']
-print(caption)
-print(caption.generate_srt_captions())
+file1.write(caption.generate_srt_captions())
+video1.streams.get_by_itag(22).download()
